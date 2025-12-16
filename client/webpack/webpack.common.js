33404ecd28c5154addec2c1eb1e3d723ce99c4b0
yjp <yangjinpeng@lawyeah.top>
2025-12-16 23:45:11 +0800

    fix: remove node_modules exclude to bundle React dependencies

--- client/webpack/webpack.common.js
+++ client/webpack/webpack.common.js
@@ -1,53 +1,53 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { generateEntries } = require('./generate-entries');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { generateEntries } = require("./generate-entries");
 
 // 动态生成 entry 配置
-const pagesDir = path.resolve(__dirname, '../src/pages');
+const pagesDir = path.resolve(__dirname, "../src/pages");
 const entries = generateEntries(pagesDir);
 
 module.exports = {
   entry: entries,
   output: {
-    path: path.resolve(__dirname, '../../dist/client'),
-    filename: '[name].js',
-    publicPath: '/static/',
+    path: path.resolve(__dirname, "../../dist/client"),
+    filename: "[name].js",
+    publicPath: "/static/",
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         use: {
-          loader: 'swc-loader',
+          loader: "swc-loader",
         },
-        exclude: /node_modules/,
+        // 移除 exclude: /node_modules/ 以允许打包 React 等依赖
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
     alias: {
-      '@': path.resolve(__dirname, '../src'),
-      '@components': path.resolve(__dirname, '../src/components'),
-      '@pages': path.resolve(__dirname, '../src/pages'),
-      '@hooks': path.resolve(__dirname, '../src/hooks'),
-      '@stores': path.resolve(__dirname, '../src/stores'),
-      '@services': path.resolve(__dirname, '../src/services'),
-      '@utils': path.resolve(__dirname, '../src/utils'),
-      '@types': path.resolve(__dirname, '../src/types'),
+      "@": path.resolve(__dirname, "../src"),
+      "@components": path.resolve(__dirname, "../src/components"),
+      "@pages": path.resolve(__dirname, "../src/pages"),
+      "@hooks": path.resolve(__dirname, "../src/hooks"),
+      "@stores": path.resolve(__dirname, "../src/stores"),
+      "@services": path.resolve(__dirname, "../src/services"),
+      "@utils": path.resolve(__dirname, "../src/utils"),
+      "@types": path.resolve(__dirname, "../src/types"),
     },
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].css',
+      filename: "[name].css",
     }),
   ],
 };
